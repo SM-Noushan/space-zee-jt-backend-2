69a42ba33056fd01b680b5b4fdc4de9bd2486a36
SMNoushan <noushan.md@gmail.com>
2025-01-28 04:30:18 +0600

    update: configs

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -6,4 +6,6 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   port: process.env.PORT || 5001,
   dbUrl: process.env.DB_URL,
+  secretToken: process.env.SECRET_TOKEN,
+  serverUrl1: process.env.SERVER_URL1,
 };
